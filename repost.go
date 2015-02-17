05cc0b56c39f16f398a302ebc7470c783b35d8f7
jacoelho <jacoelho@outlook.com>
2015-02-17 09:10:12 +0000

    well a bit better

--- repost.go
+++ repost.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-//  "fmt"
-//  "io/ioutil"
   "net/http"
-//  "net/url"
 //  "encoding/json"
   "regexp"
   "github.com/hashicorp/golang-lru"
   "log"
+  "time"
 )
 
 type WebhookResponse struct {
@@ -22,44 +20,41 @@ var (
 )
 
 func init() {
-    urlRegex = regexp.MustCompile(`<(https?([^\||>]*))>`)
-    urlCache, _  = lru.New(20)
+   urlRegex = regexp.MustCompile(`<(https?([^\||>]*))(\||>)`)
+   urlCache, _  = lru.New(20)
 }
 
 func urlMatcher(s string) (urls []string) {
-     matches := urlRegex.FindAllStringSubmatch(s, -1)
+     matches := urlRegex.FindAllString(s, -1)
      
      for _, match := range matches {
-         if match[1] != "" {
-	    urls = append(urls, match[1])
-	 }
+	urls = append(urls, match[1:len(match)-1])
      }
 
      return
 }
 
+//func urlCheck(s string) (url string) {
+//  response, err := http.Get(url)
+//}
+
 func webHookHandler(w http.ResponseWriter, r *http.Request) {
-  r.ParseForm()
-  log.Println(r.Form)
-
-  for key, _ := range r.Form {
-    log.Println(key)
-  }
-
-  incomingText := r.FormValue("text")
-  log.Printf("cenas: %d", len(incomingText))
+  incomingText := r.PostFormValue("text")
   log.Printf("Handling incoming request: %s", incomingText)
 
   urls := urlMatcher(incomingText)
   
   for _, url := range urls {
-    _, ok := urlCache.Get(url)
+    log.Printf("Checking %s", url)
+
+    raw, ok := urlCache.Get(url)
     if ok {
-      log.Printf("cenas")
-    } else {
-      log.Printf("cenas2")
-      urlCache.Add(url, "2")
+      t1, _  := time.Parse(time.RFC3339,raw.(string)) 
+      log.Printf("[REPOST] Posted %s ago", time.Since(t1))
     }
+    
+    t := time.Now()
+    urlCache.Add(url, t.Format(time.RFC3339))
   }
 }
 
